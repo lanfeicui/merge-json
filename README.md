73a05049909751ecaf9102c5b47c883037baeaa1
lanfeicui <61631740+lanfeicui@users.noreply.github.com>
2023-02-21 03:35:02 +0800

    Upload the first version

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
 # merge-json
+合并多个翻译片段文件，组合成新语言包JSON文件的扩展，适用于 [AUTOMATIC1111's stable diffusion webui](https://github.com/AUTOMATIC1111/stable-diffusion-webui)
+
+## 如何安装webUI简体中文语言包
+### 1. 通过网址安装
+- 点击 `extension` 选项卡，点击 `Install from URL` 子选项卡
+- 复制本 git 仓库网址：
+```
+https://github.com/lanfeicui/merge-json.git
+```
+- 粘贴进 URL 栏，点击 `Install`
+
+- 安装完成，跳转到 [如何使用](#如何使用)
+
+### 2. 或者，直接下载然后放在对应路径
+- [下载本 git 仓库](https://codeload.github.com/lanfeicui/merge-json/zip/refs/heads/main)为 zip 档案
+
+- 解压，并把文件夹放置在 webui 根目录下的 `extensions` 文件夹中，放好之后应该会如下图
+- 安装完成，跳转到 [如何使用](#如何使用)
+
+## 如何使用
+**重启webUI以启用扩展**
+- 在 `Extensions` 选项卡，确定已勾选本扩展☑️；如未勾选，勾选后点击**橙色按钮**启用本扩展。  
+
+**整理翻译片段json文件**
+- 建立存放翻译片段json文件的文件夹，路径无限制，举例：取名 zh_CN
+- 添加json文件。
+-- 可以根据需求，在其下面建立多个子文件夹。
+-- 文件名随意。后缀名：”json“
+-- 文件名结尾含有Overwrite.json的符合条件的json文件里面翻译词条为强制替换，主要区别是运行脚本时不会提示重复词条。
+
+**配置参数**
+- packExtension
+- packFolderPath
+- defaultJSONPath
+- outJSONPath
+
+```bash
+#语言片段文件后缀名
+packExtension = "json"
+
+#pack语言包文件夹路径(含子文件夹)
+packFolderPath = "D:\stable-diffusion-webui\extensions\merge-json\zh_CN"
+
+
+#参考的语言包json文件的路径
+#  可为空
+#defaultJSONPath = ""
+defaultJSONPath = "D:\stable-diffusion-webui\extensions\stable-diffusion-webui-localization-zh_CN\localizations\zh_CN.json"
+
+#生成新的文件路径，可以指定或空
+#  若为空：
+#    举例：
+#      outJSONPath = "", defaultJSONPath = "D:\stable-diffusion-webui\extensions\stable-diffusion-webui-localization-zh_CN\localizations\zh_CN.json"
+#      返回 D:\stable-diffusion-webui\extensions\merge-json\localizations\zh_CN-Merge-zh_CN.json
+#  若为空并且defaultJSONPath也为空：
+#    举例：
+#      outJSONPath = "", defaultJSONPath = "",当前py文件路径："D:\stable-diffusion-webui\extensions\merge-json\merge.py"
+#      返回 D:\stable-diffusion-webui\extensions\merge-json\localizations\zh_CN-AutoBuild.json
+
+#outJSONPath = "D:\stable-diffusion-webui\localizations\zh_CN_AutoBuild.json"
+outJSONPath = ""
+```
+
+**生成合并**
+手动执行 build.py ，会在指定未知生成心的语言包文件。例如：zh_CN-Merge-zh_CN.json
+
+**选择合并后的语言包（zh_CN-Merge-zh_CN）**
+- 在 `Settings` 选项卡中，找到 `Localization (requires restart)` 小项，然后在下拉选单中选中 `zh_CN-Merge-zh_CN` （如果没有就按一下🔄按钮），如图
+  
+- 然后按一下 页面顶部的  **橙色按钮** 保存设置，再按 页面底部的 **橙色按钮** 重启webUI
+
+**后期扩展翻译词条如何生效**
+每次手动执行 build.py 之后，合并后的语言包文件不能立即在web上生效。
+
+- 1. 重启stable-diffusion-webui（不建议）***
+-- 关掉stable-diffusion-webui的启动窗口，重新启动运行 webui-user.bat (或者对应启动脚本)。
+
+- 2. 刷新WebUI***
+-- 在 `Settings` 选项卡，点击 **页面顶部**的 **橙色按钮** **ReloadUI** 刷新网站
---
+++ build.py
@@ -0,0 +1,182 @@
+import os
+import sys
+import json
+import traceback
+from collections import defaultdict
+
+#参数设置#
+
+#语言片段文件后缀名
+packExtension = "json"
+
+#pack语言包文件夹路径(含子文件夹)
+packFolderPath = "D:\stable-diffusion-webui\extensions\merge-json\zh_CN"
+
+
+#参考的语言包json文件的路径
+#  可为空
+#defaultJSONPath = ""
+defaultJSONPath = "D:\stable-diffusion-webui\extensions\stable-diffusion-webui-localization-zh_CN\localizations\zh_CN.json"
+
+#生成新的文件路径，可以指定或空
+#  若为空：
+#    举例：
+#      outJSONPath = "", defaultJSONPath = "D:\stable-diffusion-webui\extensions\stable-diffusion-webui-localization-zh_CN\localizations\zh_CN.json"
+#      返回 D:\stable-diffusion-webui\extensions\merge-json\localizations\zh_CN-Merge-zh_CN.json
+#  若为空并且defaultJSONPath也为空：
+#    举例：
+#      outJSONPath = "", defaultJSONPath = "",当前py文件路径："D:\stable-diffusion-webui\extensions\merge-json\merge.py"
+#      返回 D:\stable-diffusion-webui\extensions\merge-json\localizations\zh_CN-AutoBuild.json
+
+#outJSONPath = "D:\stable-diffusion-webui\localizations\zh_CN_AutoBuild.json"
+outJSONPath = ""
+
+isInsensitive = True
+if os.path.normcase('A') == os.path.normcase('a'):
+    # case insensitive
+    isInsensitive= True
+else:
+    # case sensitive
+    isInsensitive = False
+
+#当前插件路径
+#os.path.abspath(__file__)
+currentDir = os.getcwd()
+
+if isInsensitive == True:
+    packExtension = packExtension.lower()
+
+if len(packFolderPath) == 0:
+    print("Error: packFolderPath null. Please check config.")
+    exit
+
+if len(outJSONPath) ==0:
+    packFolderName = os.path.basename(packFolderPath)
+    if len(defaultJSONPath) == 0:
+        outJSONPath = currentDir + "\\localizations\\" + packFolderName + "-AutoBuild.json"
+    else:
+        jsonName = os.path.basename(os.path.splitext(defaultJSONPath)[0])
+        outJSONPath = currentDir + "\\localizations\\" + jsonName + "-Merge-" + packFolderName +".json"
+
+def GetFileFromThisRootDir(dir,ext = None,isSubDir = True):
+    allfiles = []
+    needExtFilter = (ext != None)
+    if isSubDir == True:#遍历子文件夹
+        for root,dirs,files in os.walk(dir):
+            for filespath in files:
+                filepath = os.path.join(root, filespath)
+                extension = os.path.splitext(filepath)[1][1:]
+                if isInsensitive == True:
+                    extension = extension.lower()
+                #if needExtFilter and extension in ext:
+                if needExtFilter and (extension == ext):  # 空字符串用in 判别会有错误
+                    allfiles.append(filepath)
+                elif not needExtFilter:
+                    allfiles.append(filepath)
+    else:#不遍历子文件夹
+        for files in os.listdir(dir):
+            filepath = os.path.join(dir,files)
+            if os.path.isfile(filepath):
+                # print("是文件！")#如果是文件，则进行后缀提取，再判别
+                extension = os.path.splitext(filepath)[1][1:]
+                if isInsensitive == True:
+                    extension = extension.lower()
+                #if needExtFilter and extension in ext:
+                if needExtFilter and (extension == ext):  # 空字符串用in 判别会有错误
+                    allfiles.append(filepath)
+                elif not needExtFilter:
+                    allfiles.append(filepath)
+            # else:
+            #     print("不是文件")
+
+    return allfiles
+
+#合并词典
+def merge_dict(d1, d2, isoverwrite, istip, path):
+    dd = defaultdict(list)
+    print(path)
+    for d in (d1, d2):
+        for key, value in d.items():
+            if len(key) > 0:
+                if key in dd.keys():
+                    if istip:
+                        if isoverwrite:
+                            #各个pack文件中存在的重复词条（不含强制覆盖词条）
+                            #print("[Warning]", "Key:", key)
+                            print("Duplicate Key [Overwrite]:", key)
+                        else:
+                            #忽略原语言包中的词条，使用pack中的词条
+                            #print("[Warning, Ignore]", "Key:", key)
+                            print("Duplicate Key [Ignore]:", key)
+                    if not isoverwrite:
+                        continue
+
+                dd[key] = value
+
+    return dict(dd)
+
+pack = {}
+overwritePackList = {}
+allPack = GetFileFromThisRootDir(currentDir, packExtension, True)
+
+#读取原始语言包
+if len(defaultJSONPath) > 0:
+    if(os.path.exists(defaultJSONPath)):
+        print("Loading json file:")
+        try:
+            with open(defaultJSONPath, "r", encoding="utf8") as file:
+                data = file.read()
+                pack = merge_dict(pack, json.loads(data), True, True, defaultJSONPath)
+        except Exception as result:
+            #print(result)
+            print(f"Error loading json from {defaultJSONPath}:", file=sys.stderr)
+            print(traceback.format_exc(), file=sys.stderr)
+    else:
+        print("Error loading json from {defaultJSONPath}")
+
+print("Updateing...\r\n")
+
+#从pack模板中遍历各个翻译词条
+pack["----pack new section----"] = "----以下内容Pack新部分----"
+tmpPack = {}
+print("Loading pack files:")
+for fn in allPack:
+    if (isInsensitive == True and fn.lower() == defaultJSONPath.lower()) or (isInsensitive == False and fn == defaultJSONPath):
+        continue
+    elif (isInsensitive == True and fn.lower().endswith("overwrite." + packExtension)) or (isInsensitive == False and fn.endswith("Overwrite." + packExtension)):
+        overwritePackList[fn] = fn
+        continue
+    try:
+        with open(fn, "r", encoding="utf8") as file:
+            data = file.read()
+            tmpPack = merge_dict(tmpPack, json.loads(data), True, True, fn)
+    except Exception as result:
+        #print(result)
+        print(f"Error loading pack from {fn}:", file=sys.stderr)
+        print(traceback.format_exc(), file=sys.stderr)
+
+#覆盖已定义的词条
+pack = merge_dict(pack, tmpPack, True, False, "Updateing...\r\n")
+
+#读取强制覆盖语言包
+pack["----pack overwrite new section----"] = "----以下内容Pack强制覆盖新部分----"
+tmpOverwritePack = {}
+print("Loading Overwrite files:")
+for fn in overwritePackList:
+    if fn is not None:
+        try:
+            with open(fn, "r", encoding="utf8") as file:
+                data = file.read()
+                tmpOverwritePack = merge_dict(tmpOverwritePack, json.loads(data), True, True, fn)
+        except Exception:
+            print(f"Error loading pack from {fn}:", file=sys.stderr)
+            print(traceback.format_exc(), file=sys.stderr)
+
+#覆盖已定义的词条
+pack = merge_dict(pack, tmpOverwritePack, True, False, "Updateing...\r\n")
+
+#保存
+with open(outJSONPath, 'w', encoding='utf-8') as f:
+    json.dump(pack, f, ensure_ascii=False, indent=0, sort_keys=False)
+
+print("Finished...")
